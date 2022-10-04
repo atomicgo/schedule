3c1e4d2f9fdca014ad8eef5cd0fa3a5bc351ff99
MarvinJWendt <git@marvinjwendt.com>
2022-10-04 18:44:27 +0200

    added examples

--- schedule_test.go
+++ schedule_test.go
@@ -1,1 +1,38 @@
 package schedule
+
+import (
+	"fmt"
+	"time"
+)
+
+func ExampleAfter() {
+	task := After(5*time.Second, func() {
+		fmt.Println("5 seconds are over!")
+	})
+
+	fmt.Println("Some stuff happening...")
+
+	task.Wait()
+}
+
+func ExampleAt() {
+	task := At(time.Now().Add(5*time.Second), func() {
+		fmt.Println("5 seconds are over!")
+	})
+
+	fmt.Println("Some stuff happening...")
+
+	task.Wait()
+}
+
+func ExampleEvery() {
+	task := Every(5*time.Second, func() {
+		fmt.Println("5 seconds are over!")
+	})
+
+	fmt.Println("Some stuff happening...")
+
+	time.Sleep(10 * time.Second)
+
+	task.Stop()
+}
